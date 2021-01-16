f5b61b27588655ca756fa60d2e233a743db9eda5
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-01-16 11:35:59 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,14 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • Indosat Ooredoo [0815-1586-0089]
+│ • Telkomsel: [0813-5730-2007]
+│ • Smartfren: [0882-3543-5804]
+│ 「 Chat OWNER 」
+│ > Ingin donasi? Wa.me/6288235435804
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
-handler.tags = ['info']
+handler.tags = ['about']
 handler.command = /^dona(te|si)$/i
 
 module.exports = handler
