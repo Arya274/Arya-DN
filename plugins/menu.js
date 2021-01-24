04eea8011d0030ba407ab8d5d3af0103f1e097d3
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-01-24 16:01:04 +0700

    Update menu

--- plugins/menu.js
+++ plugins/menu.js
@@ -63,7 +63,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     let header = conn.menu.header || '╭════•›「 %category 」'
     let body   = conn.menu.body   || '┠❥ %cmd%islimit'
     let footer = conn.menu.footer || '╰═══════════════\n'
-    let after  = conn.menu.after  || conn.user.jid == global.conn.user.jid ? '' : `\nPowered by https://wa.me/${global.conn.user.jid.split`@`[0]}`
+    let after  = conn.menu.after  || conn.user.jid == global.conn.user.jid ? '' : `\nPowered by Wa.me/${global.conn.user.jid.split`@`[0]}`
     let _text  = before + '\n'
     for (let tag in groups) {
       _text += header.replace(/%category/g, tags[tag]) + '\n'
